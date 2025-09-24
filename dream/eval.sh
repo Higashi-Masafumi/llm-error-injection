afcf07b05c063b188624eec91b23c0ae9b2bef61
Masafumi Higashi <masafumi-higashi@example.com>
2025-09-24 02:38:46 +0000

    update: diffusion step数も指定するように

--- dream/eval.sh
+++ dream/eval.sh
@@ -1,8 +1,9 @@
 model_name="dream_eval"
 limit=100
 quantization="hqq"  # "hqq", "quanto", or None
-nbits=2  # 2, 4, 8 only for "hqq"
+nbits=4  # 2, 4, 8 only for "hqq"
 max_new_tokens=128
+diffusion_steps=128
 weight_and_biases_project="dream-eval"
 task="gsm8k"
 batch_size=1
@@ -12,9 +13,8 @@ export HF_ALLOW_CODE_EVAL=1
 accelerate launch eval.py \
     --model $model_name \
     --limit $limit \
-    --model_args "max_new_tokens=$max_new_tokens,quantization=$quantization,nbits=$nbits" \
+    --model_args "max_new_tokens=$max_new_tokens,quantization=$quantization,nbits=$nbits,diffusion_steps=$diffusion_steps" \
     --wandb_args "project=$weight_and_biases_project" \
     --tasks $task \
     --batch_size $batch_size \
     --confirm_run_unsafe_code
-

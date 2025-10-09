1d5694170e646f7ba8556e7376861df38d3d23b6
Masafumi Higashi <masafumi-higashi@example.com>
2025-10-09 03:05:16 +0000

    fix: 修正されたオプティマイザーの学習率を設定し、WandbLoggerを追加

--- notebooks/train-llama.py
+++ notebooks/train-llama.py
@@ -2,8 +2,9 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from pytorch_lightning.callbacks import DeviceStatsMonitor, RichProgressBar
+from pytorch_lightning.callbacks import DeviceStatsMonitor, RichProgressBar, Callback
 from pytorch_lightning.demos import Transformer, WikiText2
+from pytorch_lightning.loggers import WandbLogger
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
@@ -29,17 +30,21 @@ class LanguageModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self) -> Optimizer:
-        return torch.optim.AdamW(self.parameters(), lr=0.1)
+        return torch.optim.AdamW(self.parameters(), lr=1e-3)
 
 pl.seed_everything(42)
 dataset = WikiText2()
 train_loader = DataLoader(dataset, batch_size=64)
+wandb_logger = WandbLogger(project="language-model-training")
 
 model = LanguageModel(vocab_size=dataset.vocab_size)
 
 trainer = pl.Trainer(
     accelerator="cuda",
     devices=1,
+    logger=[wandb_logger],
+    callbacks=[RichProgressBar(), DeviceStatsMonitor()],
+    max_epochs=3
 )
 trainer.fit(
     model=model,
